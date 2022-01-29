780993a646ba982c6262de01b805ae0907b93bf2
Caio Amaral Corrêa <caiobep@me.com>
2022-01-29 10:06:56 -0300

    Feat/build individual files (#4)
    * chore: change package name
    
    * feat: improve compilation process by running lint and compiling with no emit before building
    
    * feat: support compilation of individual files, and bundling of folders
    
    github issue #2

--- scripts/build-scriptable-files.ts
+++ scripts/build-scriptable-files.ts
@@ -4,54 +4,56 @@ import { build } from 'esbuild'
 
 const sourcePath = path.join(__dirname, '..', 'src')
 
-const getFolders = async (c: string): Promise<string[]> => {
-  const filesInDirectory = await fs.readdir(c)
-
-  const folders = filesInDirectory
-    .flatMap(async f => {
-      const folderPath = path.join(sourcePath, f)
-
-      const isFolderDirectory = (
-        await fs.stat(folderPath)
-      ).isDirectory()
-
-      return (isFolderDirectory ? folderPath : [])
-    }) as unknown as string[]
-
-  return Promise.all(folders)
+const toPascalCase = (str: string): string => {
+  return str
+    .split('-')
+    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .join(' ')
 }
 
 const compileScriptFolders = async (): Promise<void> => {
-  const folders = await getFolders(sourcePath)
+  const files = await fs.readdir(sourcePath)
 
-  for await (const folder of folders) {
-    const folderName = folder.split(path.sep).pop()
+  for await (const file of files) {
+    const filePath = path.join(sourcePath, file)
+    const isDirectory = (await fs.stat(filePath)).isDirectory()
+    const scriptName = file.split(path.sep).pop() as string
+    const friendlyScriptName = toPascalCase(file).replace('ts', 'js')
 
-    console.log(`[Build] Compiling ${folderName}`)
+    console.log(`[Build] Compiling ${scriptName}`)
+
+    const outfile = `./scriptable/${friendlyScriptName}${
+      isDirectory ? '.js' : ''
+    }`
+    const entryPoints = [path.join(sourcePath, scriptName)]
 
     const program = await build({
-      entryPoints: [path.join(folder, 'index.ts')],
+      entryPoints,
+      outfile,
       minify: false,
       bundle: true,
+      allowOverwrite: true,
+      banner: {
+        js: `/// ${friendlyScriptName} - generated in ${new Date().toLocaleDateString()} /// \r\n`,
+      },
       target: 'es2019',
       platform: 'node',
-      outfile: `./scriptable/${folderName}.js`,
-      tsconfig: 'tsconfig.json'
+      tsconfig: 'tsconfig.json',
     })
 
     program.warnings.forEach(console.warn)
 
     if (program.errors.length > 0) {
       program.errors.forEach(console.error)
-      console.log(`[Build] Compiling ${folderName}.....[Failed]`)
+      console.log(`[Build] Compiling ${friendlyScriptName}.....[Failed]`)
       process.exit(1)
     }
 
-    console.log(`[Build] Compiling ${folderName}.....[OK]`)
+    console.log(`[Build] Compiling ${friendlyScriptName}.....[OK]`)
   }
 
-  console.log(`Build Succeed`)
-  process.exit(1);
+  console.log(`[Build] Succeed!`)
+  process.exit(0)
 }
 
 compileScriptFolders().then().catch(console.error)
